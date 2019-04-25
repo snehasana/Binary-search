b5b343b21c3ff5860f90cf771843b6f32fb6f490
sweety bhagat <sweety.bhagat89@gmail.com>
2019-04-25 13:05:41 +0530

    Height Of a Binary tree

---
+++ GetHeight.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+class Node {
+
+	int data;
+	Node left;
+	Node right;
+
+	Node(int data) {
+		this.left = null;
+		this.right = null;
+		this.data = data;
+	}
+
+}
+public class GetHeight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node tree = new Node(1);
+		tree.left = new Node(2);
+		tree.right = new Node(3);
+		tree.left.left = new Node(4);
+		tree.left.right = new Node(5);
+		GetHeight h = new GetHeight();
+		int height= h.getHeight(tree);
+		
+		System.out.println("Height :"+ height);
+	}
+	private int getHeight(Node root) {
+		
+		if(root == null) {
+			return 0;
+		}
+		if(root.left!=null)
+		System.out.print(root.left.data+" ");
+		int lh = getHeight(root.left);
+		if(root.right!=null)
+		System.out.println(root.right.data+" ");
+		int rh= getHeight(root.right);
+		System.out.println();
+		
+		
+		if(lh>rh)
+			return lh+1;
+		else
+			return rh+1;
+		
+	}
+	
+
+}
